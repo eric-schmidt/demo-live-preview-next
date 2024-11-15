e85e7f6597173c0a2aa46e92884e996bb0337156
Eric Schmidt <e.ryan.schmidt@gmail.com>
2024-11-15 13:27:39 -0700

    Update lib/client with streamlined version from other demos.

--- src/lib/client.js
+++ src/lib/client.js
@@ -2,101 +2,82 @@ import { createClient } from "contentful";
 import { unstable_cache } from "next/cache";
 import safeJsonStringify from "safe-json-stringify";
 
-export const getEntryById = async ({ entryId }) => {
-  const res = await fetch(
-    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_ENV_ID}/entries/${entryId}`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.CONTENTFUL_DELIVERY_KEY}`,
-      },
-    }
-  );
+// Retrieve a Contentful client with various configured options.
+export const getClient = ({ preview = false }) => {
+  try {
+    // If `preview` is true, use the Preview domain + API key, otherwise use Delivery.
+    const domain = preview ? "preview.contentful.com" : "cdn.contentful.com";
+    const apiKey = preview
+      ? process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_KEY
+      : process.env.NEXT_PUBLIC_CONTENTFUL_DELIVERY_KEY;
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
+    return createClient({
+      space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+      environment: process.env.NEXT_PUBLIC_CONTENTFUL_ENV_ID,
+      accessToken: apiKey,
+      host: domain,
+      // Content Source Maps prevent the need for manually tagging components for
+      // Live Preview Inspector Mode, but these are only available on the Preview API.
+      includeContentSourceMaps: preview,
+    });
+  } catch (error) {
+    console.error("Error initializing Contentful client:", error);
+    throw error;
   }
-
-  return await res.json();
 };
 
+// Get all entries from Contentful that are linking to a specifc entry.
 export const getLinksToEntryById = async ({ entryId }) => {
-  const res = await fetch(
-    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_ENV_ID}/entries?links_to_entry=${entryId}`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.CONTENTFUL_DELIVERY_KEY}`,
-      },
-    }
-  );
+  const client = getClient({ preview: false });
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
+  try {
+    return await client.getEntries({
+      links_to_entry: entryId,
+    });
+  } catch (error) {
+    console.error("Error fetching entries from Contentful:", error);
+    throw error;
   }
-
-  return await res.json();
 };
 
-export const getEntriesByType = async ({ preview = false, contentType }) => {
-  // Determine whether to use the preview or delivery domain + API key.
-  const domain = preview ? "preview.contentful.com" : "cdn.contentful.com";
-  const apiKey = preview
-    ? process.env.CONTENTFUL_PREVIEW_KEY
-    : process.env.CONTENTFUL_DELIVERY_KEY;
+// Get an individual entry from Contentful via its ID.
+export const getEntryById = async ({ entryId, includeDepth = 10 }) => {
+  const client = getClient({ preview: false });
 
-  const res = await fetch(
-    `https://${domain}/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_ENV_ID}/entries?content_type=${contentType}`,
-    {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    }
-  );
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
+  try {
+    const response = await client.getEntries({
+      "sys.id": entryId,
+      include: includeDepth,
+    });
+    // Only return the first item since we are querying by ID.
+    return response.items[0];
+  } catch (error) {
+    console.error("Error fetching entry:", error);
+    throw error;
   }
-
-  return await res.json();
 };
 
+// Get all entries from Contentful via their slug.
 export const getEntriesBySlug = async ({
   preview = false,
   contentType,
   slug,
   includeDepth = 10,
 }) => {
-  // Define a cached function so that we can revalidate when content is updated.
+  const client = getClient({ preview });
+  // Use the Next.js caching function so that we can revalidate when content is updated.
   const getCachedEntries = unstable_cache(
     async () => {
-      // If `preview` is true, us the Preview domain + API key, otherwise use Delivery.
-      const domain = preview ? "preview.contentful.com" : "cdn.contentful.com";
-      const apiKey = preview
-        ? process.env.CONTENTFUL_PREVIEW_KEY
-        : process.env.CONTENTFUL_DELIVERY_KEY;
-
-      const client = createClient({
-        space: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: apiKey,
-        host: domain,
-        // Content Source Maps prevent the need for manually tagging components for
-        // Live Preview Inspector Mode, but these are only available on the Preview API.
-        includeContentSourceMaps: preview,
-      });
-
       try {
         const response = await client.getEntries({
           content_type: contentType,
           include: includeDepth,
           "fields.slug": slug,
         });
-
         // Prevent circular reference errors.
         return JSON.parse(safeJsonStringify(response.items));
       } catch (error) {
-        console.error("Error fetching entries from Contentful:", error);
+        console.error("Error fetching entries:", error);
         throw error;
       }
     },
@@ -105,26 +86,29 @@ export const getEntriesBySlug = async ({
   );
 
   try {
-    const cachedData = await getCachedEntries();
-    return cachedData;
+    return await getCachedEntries();
   } catch (error) {
-    console.error("Error retrieving cached entries:", error);
+    console.error("Error fetching cached entries:", error);
     throw error;
   }
 };
 
-export const getGraphQLResponse = async ({ query }) => {
-  const response = await fetch(
-    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.CONTENTFUL_PREVIEW_KEY}`,
-      },
-      body: JSON.stringify({ query }),
-    }
-  );
+// Get all entries of a specific Content Type from Contentful.
+export const getEntriesByType = async ({
+  preview = false,
+  contentType,
+  includeDepth = 10,
+}) => {
+  const client = getClient({ preview });
 
-  return await response.json();
+  try {
+    const response = await client.getEntries({
+      content_type: contentType,
+      include: includeDepth,
+    });
+    return response.items;
+  } catch (error) {
+    console.error("Error fetching entries:", error);
+    throw error;
+  }
 };
